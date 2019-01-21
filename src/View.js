b812df2b2013abe5876b639ddd9fae4c0f271d5e
WalterDG <wadagooo@gmail.com>
2019-01-21 12:15:29 -0300

    agregando boton para visibilidad del form de agregar/editar

--- src/View.js
+++ src/View.js
@@ -2,12 +2,19 @@ import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
 
 //usamos un pre para ver el crudo en html
-const { pre, div, h1 } = hh(h);
+const { pre, div, h1, button } = hh(h);
+
+//dispatch es la fun maneja el evento click en este caso, model el dato
+function formVer(dispatch, model) {
+    return button({ className: 'f3 pv2 ph3 bg-blue white bn' },
+        'Agregar comida')
+}
 // vista. param dispach q maneja evento click, y modelo: valor contador.
-function view(dispach, model) {
+function view(dispatch, model) {
     return div({ className: 'mw6 center' }, [
         h1('Contador de calorias'),
-        pre(JSON.stringify(model, null, 2),
+        formVer(dispatch, model),
+        pre(JSON.stringify(model, null, 3),
         )]
     );
 }
